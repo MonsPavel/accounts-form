f14d2621e083b40708694c9c949438ff38beb9f9
Mons Pavel <monspavel@gmail.com>
2025-10-01 19:35:50 +0300

    add pinia, vutify, eslint

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,23 @@
 import { createApp } from 'vue'
-import './style.css'
+import { createPinia } from 'pinia';
+
+// Vuetify
+import '@mdi/font/css/materialdesignicons.css'
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+// Components
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const vuetify = createVuetify({
+  components,
+  directives,
+})
+
+const app = createApp(App);
+
+app.use(createPinia());
+app.use(vuetify);
+app.mount('#app');
